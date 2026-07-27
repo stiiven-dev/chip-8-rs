3423d35ca6a656fa21c93004e330a0d32cf49549
stiiven-dev <azizchouchen589@gmail.com>
2026-07-27 04:22:48 +0100

    feat : almost all opcodes , running 3 more test roms

--- src/cpu.rs
+++ src/cpu.rs
@@ -2,6 +2,8 @@ const CHIP8_RAM : usize = 4096;
 const REGISTER_COUNT: usize = 16;
 const STACK_SIZE: usize = 16;
 
+use rand;
+use rand::RngExt;
 use crate::font::FONT_SET;
 use crate::VIDEO_WIDTH;
 use crate::VIDEO_HEIGHT;
@@ -72,10 +74,29 @@ impl CPU {
                 self.nextpc();
                 self.clear_screen()
             },
-            (1,_,_,_) => {
+            (0,0,0xE,0xE)=>{
+                self.r#return();
+            },
+            (0x1,_,_,_) => {
                 self.nextpc();
                 self.jump_to(nnn)
             },
+            (0x2,_,_,_) => {
+                self.nextpc();
+                self.call(nnn);
+            },
+            (0x3,_,_,_)=>{
+                self.nextpc();
+                self.seq(self.v[x],nn);  //skip if equal (immediate)
+            },
+            (0x4,_,_,_) => {
+                self.nextpc();
+                self.snq(self.v[x],nn);  //skip if not equal (immediate)
+            },
+            (0x5,_,_,_) => {
+                self.nextpc();
+                self.seq(self.v[x],self.v[y]); //skip if equal
+            }
             (0x6,_,_,_) => {
                 self.nextpc();
                 self.set_reg(x,nn)
@@ -84,13 +105,95 @@ impl CPU {
                 self.nextpc();
                 self.add_reg(x.into(),nn)
             },
+            (0x8,_,_,0)=>{
+                self.nextpc();
+                self.set_reg(x,self.v[y]);
+            },
+            (0x8,_,_,1)=>{
+                self.nextpc();
+                self.or(x,y);
+            },
+            (0x8,_,_,2)=>{
+                self.nextpc();
+                self.and(x,y);
+            },
+            (0x8,_,_,3)=>{
+                self.nextpc();
+                self.xor(x,y);
+            },
+            (0x8,_,_,4)=>{
+                self.nextpc();
+                self.add(x,y);
+            },
+            (0x8,_,_,5)=>{
+                self.nextpc();
+                let rhs = self.v[x];
+                let lhs = self.v[y];
+                self.v[x] = rhs.wrapping_sub(lhs) ;
+                if rhs >= lhs {
+                    self.v[0xF] = 1;
+                }else { self.v[0xF] = 0 };
+            },
+            (0x8,_,_,6) => {
+                self.nextpc();
+                self.shr(x,y);
+            },
+            (0x8,_,_,7)=>{
+                self.nextpc();
+                let rhs = self.v[y];
+                let lhs = self.v[x];
+                self.v[x] = rhs.wrapping_sub(lhs) ;
+                if rhs >= lhs {
+                    self.v[0xF] = 1;
+                }else { self.v[0xF] = 0 };
+            },
+            (0x8,_,_,0xE) => {
+                self.nextpc();
+                self.shl(x,y);
+            },
+            (0x9,_,_,_) => {
+                self.nextpc();
+                self.snq(self.v[x],self.v[y]); //skip if not equal
+            },
             (0xA,_,_,_) => {
                 self.nextpc();
                 self.set_index(nnn);
-            }
+            },
+            (0xB,_,_,_) => {
+                self.pc = (self.v[0] as usize) + nnn ;
+            },
+            (0xC,_,_,_) => {
+                self.nextpc();
+                let mut rng = rand::rng();
+                self.v[x] = rng.random::<u8>() & nn;
+            },
             (0xD,_,_,_) => {
                 self.nextpc();
                 self.draw(x,y,n);
+            },
+            (0xF,_,1,0xE) => {
+                self.nextpc();
+                self.i += self.v[x] as usize ;
+            },
+            (0xF,_,3,3) => {
+                self.nextpc();
+                let val = self.v[x];
+                self.ram[self.i] = val / 100 ;
+                self.ram[self.i +1] = (val % 100)/ 10 ;
+                self.ram[self.i +2] = val % 10 ;
+
+            },
+            (0xF,_,5,5) => {
+                self.nextpc();
+                for i in 0..x + 1 {
+                    self.ram[self.i + i] = self.v[i];
+                }
+            },
+            (0xF,_,6,5) => {
+                self.nextpc();
+                for i in 0..x + 1 {
+                    self.v[i] = self.ram[self.i + i];
+                }
             }
             (_, _, _, _) => {}
         }
@@ -121,8 +224,8 @@ impl CPU {
         self.i = nnn;
     }
 
-    fn set_reg(&mut self , x : usize , nn : u8) {
-        self.v[x] = nn;
+    fn set_reg(&mut self , x : usize , y : u8) {
+        self.v[x] = y;
     }
 
     fn add_reg(&mut self , x : usize , nn:u8) {
@@ -133,6 +236,61 @@ impl CPU {
         self.pc = j ;
     }
 
+    fn call(&mut self, addr:usize) {
+        self.stack[self.stack_pointer] = self.pc as u16 ;
+        self.stack_pointer+=1;
+        self.pc = addr
+    }
+
+    fn r#return(&mut self) {
+        self.stack_pointer-=1;
+        self.pc = self.stack[self.stack_pointer].into();
+
+    }
+
+    fn seq(&mut self,x:u8,y:u8){
+        if x == y {
+            self.pc+=2;
+        }
+    }
+
+    fn snq(&mut self,x:u8,y:u8){
+        if x != y {
+            self.pc+=2;
+        }
+    }
+
+    pub fn or(&mut self , x:usize , y:usize){
+        self.v[x] = self.v[x] | self.v[y];
+    }
+
+    pub fn and(&mut self , x:usize , y:usize){
+        self.v[x] = self.v[x] & self.v[y];
+    }
+    #[allow(unused_comparisons)]
+    pub fn add(&mut self , x:usize , y:usize){
+        let vx = self.v[x] as u16;
+        let vy = self.v[y] as u16;
+        let result = vx + vy;
+        self.v[x] = result as u8;
+        self.v[0x0f] = if result > 0xFF { 1 } else { 0 };
+    }
+    pub fn xor(&mut self , x:usize , y:usize){
+        self.v[x] = self.v[x] ^ self.v[y]
+    }
+
+    pub fn shr(&mut self, x:usize,y:usize){
+        self.v[x]=self.v[y];
+        let carry = self.v[x] & 1;
+        self.v[x] >>= 1;
+        self.v[0xF]=carry;
+    }
+    pub fn shl(&mut self, x:usize,y:usize){
+        self.v[x]=self.v[y];
+        let carry = (self.v[x] & 0x80) >> 7  ;
+        self.v[x] <<= 1;
+        self.v[0xF]=carry;
+    }
     fn clear_screen(&mut self) {
         for y in 0..VIDEO_HEIGHT {
             for x in 0..VIDEO_WIDTH {
